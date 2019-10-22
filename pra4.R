80322aac411b837bc7b5a64079e8e202cf48f7ba
A. Leung <aleungyp@gmail.com>
2019-10-22 20:38:16 +0100

    5th

--- pra4.R
+++ pra4.R
@@ -1,4 +1,5 @@
 # 1. save the file
 # 2. commit your change (still not in github, in git only)
 # 3. press push to the github
-# if pushed, but not committed, no change in github
+# 4. if pushed, but not committed, no change in github
+# 5. remember to save before comitt and push
